4f7674b0ff365a43e7be163630014b491e4626dd
Ibironke Yekinni <35920144+ykthinkbig@users.noreply.github.com>
2020-03-21 19:52:12 +0100

    Testify Feedback
    Task tested and confirmed successful script. Well done. 
    
    To keep your task readable:
    > Try spacing out your codes
    > Enhance your code structure and
    > Add comments just like I did
    
    Overall, I will say a big well done. This is a good way to start.
    
    Revisit the other task, update it with this comments and notify your me

--- src/test/java/Login/LoginTests.java
+++ src/test/java/Login/LoginTests.java
@@ -10,17 +10,25 @@ public class LoginTests {
 
     private WebDriver driver;
     public void setUp() throws InterruptedException {
+        //launch chromebrower
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
+        //inout the URL
         driver.get("https://www.facebook.com/");
+        //waiting for the page URL to fully load
         Thread.sleep(5000);
+        //maximixe opened page
         driver.manage().window().maximize();
+        //get page title
         System.out.println(driver.getTitle());
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.id("email")).sendKeys("ridwanconcept@yahoo.com");
-        driver.findElement(By.id("pass")).sendKeys("Labibah20.");
+        //input username
+        driver.findElement(By.id("email")).sendKeys("xxxxxxx");
+        //input passeword
+        driver.findElement(By.id("pass")).sendKeys("xxxxxxxxx");
+        //click on the login button
         driver.findElement(By.id("loginbutton")).click(); }
-
+    //run Test
     public static void main(String args[]) throws InterruptedException {
         LoginTests test = new LoginTests();
         test.setUp();
